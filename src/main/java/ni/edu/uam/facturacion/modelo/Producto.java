2ffb3b1e01fb9094a93ccbee43105fb9dbe79673
alee1616 <maguillenc@uamv.edu.ni>
2026-05-29 16:09:42 -0600

    feat: Agregar más campos a la clase Producto

--- src/main/java/ni/edu/uam/facturacion/modelo/Producto.java
+++ src/main/java/ni/edu/uam/facturacion/modelo/Producto.java
@@ -3,21 +3,36 @@ package ni.edu.uam.facturacion.modelo;
 
 import lombok.Getter;
 import lombok.Setter;
-import org.openxava.annotations.Required;
+import org.openxava.annotations.*;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.Id;
+import javax.persistence.*;
+import java.math.BigDecimal;
 
 @Entity
 @Getter @Setter
 public class Producto {
 
     @Id
-    @Column(length = 6)
+    @Column(length = 9)
     int numero;
 
     @Column(length = 50)
     @Required
     String descripcion;
+
+    @ManyToOne(
+            fetch=FetchType.LAZY,
+            optional=true)
+    @DescriptionsList
+    Categoria categoria;
+
+    @Money
+    BigDecimal precio;
+
+    @Files
+    @Column(length=32)
+    String fotos;
+
+    @TextArea
+    String remarcas;
 }

891aff370cafb51fc160c8f60b9a338a8843066e
alee1616 <maguillenc@uamv.edu.ni>
2026-05-29 15:45:03 -0600

    feat: primeros pasos en openxava y creacion de Clases, Categoria

---
+++ src/main/java/ni/edu/uam/facturacion/modelo/Categoria.java
@@ -0,0 +1,30 @@
+package ni.edu.uam.facturacion.modelo;
+
+
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.annotations.GenericGenerator;
+import org.openxava.annotations.Hidden;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+public class Categoria {
+
+    @Entity   @Getter @Setter
+    public class Catergory {
+
+        @Id
+        @Hidden
+        @GeneratedValue(generator = "system-uuid")
+        @GenericGenerator(name = "system-uuid", strategy = "uuid")
+        @Column (length = 32)
+        String oid;
+
+        @Column(length = 50)
+        String descripcion;
+    }
+
+}

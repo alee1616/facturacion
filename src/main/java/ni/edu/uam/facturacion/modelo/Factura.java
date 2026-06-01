c21a5d3d2290122b0ffa871685e34895c907e83a
alee1616 <maguillenc@uamv.edu.ni>
2026-05-31 20:18:11 -0600

    feat: Agregar un paquete CalculadorSiguienteNumeroParaAnyo y crear una clase para detalle para la factura

--- src/main/java/ni/edu/uam/facturacion/modelo/Factura.java
+++ src/main/java/ni/edu/uam/facturacion/modelo/Factura.java
@@ -2,18 +2,13 @@ package ni.edu.uam.facturacion.modelo;
 
 import lombok.Getter;
 import lombok.Setter;
+import ni.edu.uam.facturacion.calculadores.CalculadorSiguienteNumeroParaAnyo;
 import org.hibernate.annotations.GenericGenerator;
-import org.openxava.annotations.DefaultValueCalculator;
-import org.openxava.annotations.Hidden;
-import org.openxava.annotations.Required;
-import org.openxava.annotations.Stereotype;
+import org.openxava.annotations.*;
 import org.openxava.calculators.CurrentLocalDateCalculator;
 import org.openxava.calculators.CurrentYearCalculator;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
+import javax.persistence.*;
 import java.time.LocalDate;
 
 @Entity @Getter @Setter
@@ -31,11 +26,19 @@ public class Factura {
     int anyo;
 
     @Column(length = 6)
+    @DefaultValueCalculator(value = CalculadorSiguienteNumeroParaAnyo.class,
+    properties = @PropertyValue(name = "anyo"))
     int numero;
 
     @Required
     @DefaultValueCalculator(CurrentLocalDateCalculator.class)
     LocalDate fecha;
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    Cliente cliente;
+
+    @ElementCollection
+    Collection<Detalle> detalles;
 
     @Stereotype("MEMO")
     String observaciones;
